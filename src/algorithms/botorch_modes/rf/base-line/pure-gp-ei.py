24dd54eb58c61ad106940d37f37c88bcd1f66c7c
xiaojin <xiaojin7656@gmail.com>
2022-02-17 20:11:45 +0100

    model upload

---
+++ src/algorithms/botorch_modes/rf/base-line/pure-gp-ei.py
@@ -0,0 +1,231 @@
+import torch
+import os
+import csv
+import sys
+from botorch import fit_gpytorch_model
+from botorch.acquisition.monte_carlo import qExpectedImprovement, qNoisyExpectedImprovement
+from botorch.acquisition.analytic import ExpectedImprovement, NoisyExpectedImprovement
+from botorch.sampling.samplers import SobolQMCNormalSampler
+from botorch.exceptions import BadInitialCandidatesWarning
+import time
+import getopt
+
+from botorch.models import SingleTaskGP, FixedNoiseGP, ModelListGP
+from gpytorch.mlls import ExactMarginalLogLikelihood
+
+from botorch.optim import optimize_acqf
+
+import random
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+dtype = torch.double
+folder_name = 'rf_mnist_continue'
+
+def call_external(X, num_trial, num_param, dataset, node_id):
+    num_node = 1
+    dataset = int(dataset)
+    array_results = []
+    hp = X.cpu().numpy()
+    for i in range (0, num_trial):
+        current_time = time.time()
+        csv_file_name = '/home/jjshi/modes/botorch/' + folder_name + '/base-line/dataset' + str(dataset) + '_resluts_' + str(
+        current_time) + '.csv'
+        csv_file_name_2 = '/home/jjshi/modes/botorch/' + folder_name + '/base-line/dataset' + str(dataset) + '_resluts_' + str(
+        current_time) + '.csv'
+
+        with open(csv_file_name_2, 'w') as csvFile:
+            writer = csv.writer(csvFile)
+        csvFile.close()
+
+        hp_param = ''
+        for j in range(0, num_param):
+            hp_param = hp_param + str(hp[i][j])
+            if (j < num_param - 1):
+                hp_param = hp_param + '_'
+        #print (hp_param)
+
+        commands = 'python3 /home/jjshi/modes/botorch/' + folder_name + '/call-ml-idv.py' + ' -i ' + str(
+            hp_param) + ' -o ' + str(csv_file_name) + ' -d ' + str(node_id) + ' -s ' + str(dataset) + ' &'
+        os.system(commands)
+
+        finished = 0
+
+        while (finished == 0):
+            with open(csv_file_name_2, 'r') as csvfile:
+                data = [row for row in csv.reader(csvfile)]
+            csvfile.close()
+            #print('current finished set: ', len(data))
+            if (len(data) < (num_node)):
+                time.sleep(10)
+            else:
+                finished = 1
+
+        with open(csv_file_name_2, 'r') as csvfile:
+            data_result = [row for row in csv.reader(csvfile)]
+        csvfile.close()
+
+        result = data_result[0][0]
+
+        array_results.append(float(result))
+
+    tensor_results = torch.tensor(array_results, device=device, dtype=dtype)
+
+    return tensor_results
+
+
+def generate_initial_data(dataset, node_id):
+    # generate training data
+    train_x = generate_rand(20, 7)
+    exact_obj = call_external(train_x, 20, 7, dataset, node_id)
+    train_obj = exact_obj.unsqueeze(-1)
+    best_tensor_value, indices = torch.max(exact_obj, 0)
+    best_observed_value = best_tensor_value.item()
+    current_best_config = exact_obj[indices].item()
+
+    return train_x, train_obj, best_observed_value, current_best_config
+
+    
+def initialize_model(train_x, train_obj, state_dict=None):
+    model = SingleTaskGP(train_x, train_obj)
+    mll = ExactMarginalLogLikelihood(model.likelihood, model)
+    # load state dict if it is passed
+    if state_dict is not None:
+        model.load_state_dict(state_dict)
+    return mll, model
+
+BATCH_SIZE = 1
+bounds = torch.tensor([[0, 0, 0, 2, 1, 0, 0], [149.99, 2.99, 40.99, 20.99, 20.99, 1.99, 1.99]], device=device, dtype=dtype)
+
+
+def generate_rand (sets, size):
+    random_set = []
+
+    for i in range(0, sets):
+        data = []
+        data.append(random.randint(0, 149))
+        data.append(random.randint(0, 2))
+        data.append(random.randint(0, 40))
+        data.append(random.randint(2, 20))
+        data.append(random.randint(1, 20))
+        data.append(random.randint(0, 1))
+        data.append(random.randint(0, 1))
+
+        random_set.append(data)
+
+    return torch.tensor(random_set, device=device, dtype=dtype)
+
+
+
+
+def optimize_acqf_and_get_observation(acq_func, dataset, node_id):
+    """Optimizes the acquisition function, and returns a new candidate and a noisy observation."""
+    # optimize
+    new_x, _ = optimize_acqf(
+        acq_function=acq_func,
+        bounds=bounds,
+        q=BATCH_SIZE,
+        num_restarts=10,
+        raw_samples=512,  # used for intialization heuristic
+        options={"batch_limit": 5, "maxiter": 200},
+    )
+    # observe new values
+
+    exact_obj = call_external(new_x, BATCH_SIZE, 7, dataset, node_id).unsqueeze(-1)  # add output dimension
+    new_obj = exact_obj
+    return new_x, new_obj
+
+N_TRIALS = 10
+N_BATCH = 100
+MC_SAMPLES = 256
+
+def main(argv):
+    dataset = 1
+    node_id = 1
+    try:
+        opts, args = getopt.getopt(argv, "hd:n:", ["dataset=", "nodeid="])
+    except getopt.GetoptError:
+        print ('random parallel with input dataset')
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print ('random parallel with input dataset')
+            sys.exit()
+        elif opt in ("-d", "--dataset"):
+            dataset = int(arg)
+        elif opt in ("-n", "--nodeid"):
+            node_id = int(arg)
+    # average over multiple trials
+    for trial in range(1, N_TRIALS + 1):
+
+        print(f"\nTrial {trial:>2} of {N_TRIALS} ", end="")
+        best_observed_ei, best_observed_nei = [], []
+
+        # initialize the best configuration
+        # best_observed_ei_conf, best_observed_all_nei_conf, best_random_conf = [], [], []
+
+        # call helper functions to generate initial training data and initialize model
+        train_x_ei, train_obj_ei, best_observed_value_ei, current_best_config = generate_initial_data(dataset, node_id)
+        mll_ei, model_ei = initialize_model(train_x_ei, train_obj_ei)
+
+        best_observed_ei.append(best_observed_value_ei)
+
+        # run N_BATCH rounds of BayesOpt after the initial random batch
+        for iteration in range(1, N_BATCH + 1):
+
+            # fit the models
+            fit_gpytorch_model(mll_ei)
+
+            # for best_f, we use the best observed noisy values as an approximation
+            EI = ExpectedImprovement(
+                model=model_ei,
+                best_f=train_obj_ei.max(),
+            )
+            # optimize and get new observation
+            new_x_ei, new_obj_ei = optimize_acqf_and_get_observation(EI, dataset, node_id)
+
+            # update training points
+            train_x_ei = torch.cat([train_x_ei, new_x_ei])
+            train_obj_ei = torch.cat([train_obj_ei, new_obj_ei])
+
+            # update progress
+
+            best_value_ei = train_obj_ei.max().item()
+            best_observed_ei.append(best_value_ei)
+
+            # reinitialize the models so they are ready for fitting on next iteration
+            # use the current state dict to speed up fitting
+            mll_ei, model_ei = initialize_model(
+                train_x_ei,
+                train_obj_ei,
+                model_ei.state_dict(),
+            )
+
+        # return the best configuration
+        best_tensor_ei, indices_ei = torch.max(train_obj_ei, 0)
+        train_best_x_ei = train_x_ei[indices_ei].cpu().numpy()
+
+
+        from botorch.acquisition import PosteriorMean
+
+        argmax_pmean_ei, max_pmean_ei = optimize_acqf(
+            acq_function=PosteriorMean(model_ei),
+            bounds=bounds,
+            q=1,
+            num_restarts=20,
+            raw_samples=2048,
+        )
+
+        csv_file_name = '/home/jjshi/modes/botorch/' + folder_name + '/base-line/hp-gp-ei-dataset-' + str(
+            dataset) + '-node-' + str((node_id+1)) + '-trail' + str(trial) + '.csv'
+
+        with open(csv_file_name, 'w') as csvFile:
+            writer = csv.writer(csvFile)
+            writer.writerow([str(argmax_pmean_ei.cpu().numpy()), str(max_pmean_ei.cpu().numpy())]) # ei prediction
+            writer.writerow([str(train_best_x_ei), str(best_tensor_ei.cpu().numpy())]) # ei observation
+
+        csvFile.close()
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
+
+
---
+++ src/algorithms/botorch_modes/rf/rf_func.py
@@ -0,0 +1,53 @@
+# bj rf test
+from sklearn.ensemble import RandomForestClassifier
+import timeit
+from Hiwi.src.algorithms.botorch_modes.rf.dataloader import load_data
+
+
+# from dataloader import load_data
+
+def rf_model(hyper_vector, datanum):
+    # pool of hypers
+    trees = hyper_vector[0] + 1
+    features = ['auto', 'sqrt', 'log2']
+    if (hyper_vector[2] == 0):
+        depth = None
+    else:
+        depth = hyper_vector[2]
+    split = hyper_vector[3]
+    leaf = hyper_vector[4]
+    fuc = ['gini', 'entropy']
+    boots = ['True', 'False']
+
+    # Build model
+    rf = RandomForestClassifier(n_estimators=trees,
+                                max_features=features[hyper_vector[1]],
+                                max_depth=depth,
+                                min_samples_split=split,
+                                min_samples_leaf=leaf,
+                                criterion=fuc[hyper_vector[5]],
+                                bootstrap=boots[hyper_vector[6]],
+                                random_state=0,
+                                n_jobs=-1)
+
+    # Training and Validating
+    X_train, y_train, X_val, y_val = load_data(datanum, 'test')
+    start = timeit.default_timer()
+
+    rf.fit(X_train, y_train)
+    score_val = rf.score(X_val, y_val)
+    stop = timeit.default_timer()
+    time = stop - start
+
+    # Results
+    # print('Training Time: ',stop - start)
+    # print("Training set score: %f" % mlp.score(X_train, y_train))
+    # print("Test set score: %f" % mlp.score(X_test, y_test))
+
+    return time, score_val
+
+
+# hyper_vector = [int(61.03794141), int(1.66359048), int(16.621841), int(14.05320706), int(13.98009677), int(1.00152747),
+#    int(0.96022582)]
+# time, scores = rf_model(hyper_vector, 0, 0)
+# print(time, scores)
