dbdcd5bb3dd332d8ef2914f9d8f56b4c241201e2
jinVenus <56721695+jinVenus@users.noreply.github.com>
2023-02-14 17:23:42 +0100

    delete Unnecessary infomations

--- src/botorch_modes/mlp/dataloader.py
+++ src/botorch_modes/mlp/dataloader.py
@@ -11,7 +11,7 @@ def load_data(datasets_num, type):
     dataset = datasets[datasets_num]
 
     if dataset == 'dynamic':
-        data1 = pd.read_csv('E:\\ML\\Hiwi\\data\\MNIST\\raw\\mnist_dynamic.csv', sep=',', header=None)
+        data1 = pd.read_csv('..\\data\\mnist_dynamic.csv', sep=',', header=None)
 
         X_train1 = data1.iloc[:, 1:]
         y_train1 = data1.iloc[:, 0]
@@ -40,22 +40,15 @@ def load_data(datasets_num, type):
         print(X_train.shape)
 
     elif dataset == 'static':
-        data1 = pd.read_csv('E:\\ML\\Hiwi\\data\\MNIST\\raw\\mnist_cd_fr15.csv', sep=',', header=None)
-        data2 = pd.read_csv('E:\\ML\\Hiwi\\data\\MNIST\\raw\\mnist_cd_fr0.csv', sep=',', header=None)
+        data1 = pd.read_csv('..\\data\\mnist_cd_fr15.csv', sep=',', header=None)
 
         X_train1 = data1.iloc[:, 1:]
         y_train1 = data1.iloc[:, 0]
         X_train1 = np.array(X_train1)
         y_train1 = np.array(y_train1)
 
-        X_train2 = data2.iloc[:, 1:]
-        y_train2 = data2.iloc[:, 0]
-        X_train2 = np.array(X_train2)
-        y_train2 = np.array(y_train2)
-
         if type == 'eval':
             X_train1 = X_train1.reshape(len(data1), -1)
-            X_train2 = X_train2.reshape(len(data2), -1)
             X_test = X_train1[50000:]
             X_val = X_train1[40000:50000]
             X_train = X_train1[:40000]
@@ -65,7 +58,6 @@ def load_data(datasets_num, type):
 
         if type == 'test':
             X_train1 = X_train1.reshape(len(data1), -1)
-            X_train2 = X_train2.reshape(len(data2), -1)
             X_val = X_train1[40000:50000]
             X_test = X_train1[50000:]
             X_train = X_train1[:40000]
